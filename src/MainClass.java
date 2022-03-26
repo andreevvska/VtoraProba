083fd29839b3fdf8d86ca2094a1d1f86f5c5c094
andreevvska <ivanaand89@gmail.com>
2022-03-27 00:19:51 +0100

    Complete commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,5 +2,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("master branch");
         System.out.println("Vtornik_15 branch");
+        System.out.println("This should be modified actually");
+        System.out.println("This is now okay");
     }
 }
